447ebb66e7dbc57900a359fb6d100073b1c53606
shafi <frshafi49@gmail.com>
2021-01-04 02:56:44 +0600

    modified home page

---
+++ components/home/HomeSlideProduct.vue
@@ -0,0 +1,64 @@
+<template>
+<v-card   elevation="5" rounded >
+    <v-sheet
+    class="mx-auto ma-1"
+  >
+    <v-slide-group
+      v-model="model"
+      class="pa-1"
+
+
+    >
+      <v-slide-item
+        v-for="n in 10"
+        :key="n"
+      >
+
+  <v-card
+    hover
+    class="ma-2"
+    max-width="150"
+    max-hight="100"
+  >
+    <v-img
+    contain
+    class="green lighten-4"
+      src="https://firebasestorage.googleapis.com/v0/b/image-server-65bbd.appspot.com/o/restaurant%2Fimage_1609620325390.png?alt=media&amp;token=0e18dc48-ed57-468d-89bb-9625efa31da3&quot"
+      height="100"
+    >
+
+    </v-img>
+
+
+    <v-card-title>
+      Top western road trips
+    </v-card-title>
+    <v-card-subtitle>
+      1,000 miles of wonder
+    </v-card-subtitle>
+
+    <v-card-actions>
+    </v-card-actions>
+  </v-card>
+
+      </v-slide-item>
+    </v-slide-group>
+  </v-sheet>
+</v-card>
+
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      model: null,
+    }),
+  }
+</script>
+
+<style>
+
+
+
+
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,116 +1,20 @@
 <template>
-  <v-container>
-    <!-- notice starts -->
-    <v-row>
-      <v-col>
-       
-      </v-col>
-    </v-row>
-    <!-- notice ends -->
-
-    <!-- Special Menu starts -->
-    <v-row>
-      <v-col class="pb-0">
-        <p class="text-left display-1 text-md-center">Nawras Special</p>
-        <br>
-      </v-col>
-    </v-row>
-    <v-sheet class="mx-auto" elevation="2">
-      <v-slide-group
-        v-model="model"
-        class="pa-4"
-        active-class="success"
-        show-arrows
-      >
-        <v-slide-item
-          v-for="n in 15"
-          :key="n"
-          v-slot:default="{ active, toggle }"
-        >
-          <v-card
-            :color="active ? undefined : 'grey lighten-1'"
-            class="ma-4"
-            height="200"
-            width="100"
-            @click="toggle"
-          >
-            <v-row class="fill-height" align="center" justify="center">
-              <v-scale-transition>
-                <v-icon
-                  v-if="active"
-                  color="white"
-                  size="48"
-                  v-text="'mdi-close-circle-outline'"
-                ></v-icon>
-              </v-scale-transition>
-            </v-row>
-          </v-card>
-        </v-slide-item>
-      </v-slide-group>
-    </v-sheet>
-    <!-- Special Menu ends -->
-
-    <!-- promo starts -->
-    <v-row>
-      <v-col>
-        <v-card class="d-inline-block mx-auto" width="100%">
-
-          
-        
-        </v-card>
-      </v-col>
-    </v-row>
-    <!-- promo ends -->
-
-    <!-- photo gallery starts -->
-    <v-row>
-      <v-col class="pb-0">
-        <h3 class="left display-1 text-md-center" >Photo Gallery</h3>
-        <br>
-      </v-col>
-      
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-carousel height="420">
-          <v-carousel-item
-            v-for="(i, j) in 4"
-            :key="i"
-            :src="`/img/foods/ruya${j + 1}.jpg`"
-          >
-            <v-row class="fill-height" align="center" justify="center">
-              <div id="sliderText" class="d-flex flex-column slider-background">
-                <!-- <h1 style="font-size:10vw;">Responsive Text</h1> -->
-                <span class="display-3 white--text"
-                  >Photo heading</span
-                >
-                <span class="display-1 white--text"
-                  >Photo sub heading</span
-                >
-              </div>
-            </v-row>
-          </v-carousel-item>
-        </v-carousel>
-      </v-col>
-    </v-row>
-    <!-- photo gallery ends -->
-  </v-container>
+<div>
+    <HomeIntroOne/>
+    <HomeColumn/>
+   <HomeSlideProduct/>
+</div>
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
-import VuetifyLogo from "~/components/VuetifyLogo.vue";
-
+import HomeIntro from "@/components/home/HomeIntro.vue";
+import HomeIntroOne from "@/components/home/HomeIntroOne.vue";
+import HomeColumn from "@/components/home/HomeColumn.vue";
+import HomeSlideProduct from "@/components/home/HomeSlideProduct.vue";
 export default {
-  components: {
-    Logo,
-    VuetifyLogo,
-  },
+  components: { HomeIntro, HomeIntroOne, HomeColumn, HomeSlideProduct },
   data() {
-    return {
-      model: 0,
-      colors: ["primary", "secondary", "yellow darken-2"],
-    };
+    return {};
   },
 };
 </script>
