023f72864f1504136d2b9c976a28d53b6a61cebf
SMASobur <sikdar.se@gmail.com>
2021-04-06 01:49:47 +0200

    contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,43 @@
-<template>
+/* <template>
 <div class="ml-5"><h1>Contact Us</h1> <br> <p>Vi importerar livsmedelsprodukter från alla världsdelar bland annat
 Turkiet, Kina, Mexiko, Indien, Pakistan, Portugal, Argentina, Egypt, Kanada..</p></div>
 
 
+</template> */
+
+
+<template>
+      <v-row justify="center">
+    <v-col
+      cols="12"
+      sm="10"
+      md="10"
+      lg="12"
+    >
+      <v-sheet
+        elevation="10"
+        rounded="xl"
+      >
+        <v-sheet
+          class="pa-3 primary text-right"
+          dark
+          rounded="t-xl"
+        >
+        <div class="text-center font-weight-bold text-uppercase" >
+            Contact Us
+            <v-btn
+                class="ml-2"
+                icon
+            >
+                <v-icon>mdi-card-account-phone</v-icon>
+            </v-btn>
+          </div>
+        </v-sheet>
+
+        <div class="pa-4">
+         text here
+        </div>
+      </v-sheet>
+    </v-col>
+  </v-row>
 </template>
