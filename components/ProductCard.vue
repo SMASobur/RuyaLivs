d1e26e9467d206a5aed36fbaf27be5147a6bd315
SMASobur <sikdar.se@gmail.com>
2021-04-15 22:02:46 +0200

    quantity added into frontend

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -50,7 +50,7 @@
             :class="{ 'mx-2': hasDiscount }"
             class="success--text my-0 font-weight-bold text-truncate"
           >
-            SEK {{ price }}
+            SEK {{ (price/qnt).toFixed(2)}}
           </p>
         </div>
       </v-card-text>
@@ -92,7 +92,11 @@ export default {
       default: "Food",
     },
     price: {
-      type: String,
+      type: String | Number,
+      required: true,
+    },
+      qnt: {
+      type: String | Number,
       required: true,
     },
     originalPrice: {
